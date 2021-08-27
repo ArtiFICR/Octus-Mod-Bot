e72585cac10b43a3e466f2ed9cb6766542610070
ArtiFICR <49602543+ArtiFICR@users.noreply.github.com>
2021-08-27 08:23:11 -0500

    Update displayCard.py

--- displayCard.py
+++ displayCard.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 
 async def displayCard(ctx, list_of_cards, card_to_draw):
     file = discord.File(
-        f'C:/Users/bowab/Documents/Coding Stuff/Discord-Bots/live-octus-bot/playing-cards/{list_of_cards[card_to_draw]}', filename="image.png")
+        f'C:/PATH_TO_CARD_IMAGES/{list_of_cards[card_to_draw]}', filename="image.png")
     embed_msg = discord.Embed(title='Card Drawer:', color=0x179142)
     embed_msg.add_field(name='Here is the card I drew from the deck',
                         value='...', inline=False)
